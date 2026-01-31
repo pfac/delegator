ac2eb7860c184119fca6fd3ceaa3be067d5b697f
Pedro Costa <pfac@users.noreply.github.com>
2026-01-31 22:36:19 +0000

    Delegate all functions in multiple modules

--- lib/delegator.ex
+++ lib/delegator.ex
@@ -1,19 +1,28 @@
 defmodule Delegator do
   defmacro __using__(opts) do
-    target = Keyword.fetch!(opts, :to)
-    functions = target |> Macro.expand(__CALLER__) |> Kernel.apply(:__info__, [:functions])
-    pos_ints = Stream.iterate(1, &(&1 + 1))
+    targets =
+      case Keyword.fetch!(opts, :to) do
+        nil -> raise ArgumentError, ":to is required"
+        [] -> raise ArgumentError, ":to must not be empty"
+        [_ | _] = targets -> targets
+        {_, _, _} = target -> [target]
+      end
 
-    for {fun_name, fun_arity} <- functions do
-      fun_args =
-        pos_ints
-        |> Stream.take(fun_arity)
-        |> Stream.map(&"arg#{&1}")
-        |> Stream.map(&String.to_atom/1)
-        |> Enum.map(&{&1, [], nil})
+    for target <- targets do
+      functions = target |> Macro.expand(__CALLER__) |> Kernel.apply(:__info__, [:functions])
+      pos_ints = Stream.iterate(1, &(&1 + 1))
 
-      quote do
-        defdelegate unquote(fun_name)(unquote_splicing(fun_args)), to: unquote(target)
+      for {fun_name, fun_arity} <- functions do
+        fun_args =
+          pos_ints
+          |> Stream.take(fun_arity)
+          |> Stream.map(&"arg#{&1}")
+          |> Stream.map(&String.to_atom/1)
+          |> Enum.map(&{&1, [], nil})
+
+        quote do
+          defdelegate unquote(fun_name)(unquote_splicing(fun_args)), to: unquote(target)
+        end
       end
     end
   end
--- test/delegator_test.exs
+++ test/delegator_test.exs
@@ -1,21 +1,43 @@
 defmodule DelegatorTest do
   use ExUnit.Case
 
-  defmodule Target do
+  defmodule A do
     def a, do: 1
+    def a(_), do: 2
+    def a(_, _), do: 3
+    def a(_, _, _), do: 4
+  end
+
+  defmodule B do
+    def b, do: 1
     def b(_), do: 2
-    def c(_, _), do: 3
-    def d(_, _, _), do: 4
+    def b(_, _), do: 3
+    def b(_, _, _), do: 4
   end
 
-  defmodule Wrapper do
-    use Delegator, to: DelegatorTest.Target
+  defmodule WrapAllOfOne do
+    use Delegator, to: DelegatorTest.A
   end
 
-  test "delegates all functions in target" do
-    assert Wrapper.a() == 1
-    assert Wrapper.b(1) == 2
-    assert Wrapper.c(1, 2) == 3
-    assert Wrapper.d(1, 2, 3) == 4
+  test "delegates all functions to a single module" do
+    assert WrapAllOfOne.a() == 1
+    assert WrapAllOfOne.a(1) == 2
+    assert WrapAllOfOne.a(1, 2) == 3
+    assert WrapAllOfOne.a(1, 2, 3) == 4
+  end
+
+  defmodule WrapAllOfMany do
+    use Delegator, to: [DelegatorTest.A, DelegatorTest.B]
+  end
+
+  test "delegates all functions to multiple modules" do
+    assert WrapAllOfMany.a() == 1
+    assert WrapAllOfMany.a(1) == 2
+    assert WrapAllOfMany.a(1, 2) == 3
+    assert WrapAllOfMany.a(1, 2, 3) == 4
+    assert WrapAllOfMany.b() == 1
+    assert WrapAllOfMany.b(1) == 2
+    assert WrapAllOfMany.b(1, 2) == 3
+    assert WrapAllOfMany.b(1, 2, 3) == 4
   end
 end
