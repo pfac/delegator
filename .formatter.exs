74d343ef2bcd0808d238d3a4dc215294d34181c4
Pedro Costa <pfac@users.noreply.github.com>
2026-02-08 00:22:13 +0000

    Export locals_without_parens in formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,18 +1,22 @@
-# Used by "mix format"
+locals_without_parens = [
+  defdelegateall: 1,
+  defdelegateall: 2,
+  defdelegateallmacros: 1,
+  defdelegateallmacros: 2,
+  defdelegateeverything: 1,
+  defdelegateeverything: 2,
+  defdelegatemacro: 2,
+  refute_defined: 2,
+  refute_defined: 3,
+  refute_macro: 2,
+  refute_macro: 3
+]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    defdelegateall: 1,
-    defdelegateall: 2,
-    defdelegateallmacros: 1,
-    defdelegateallmacros: 2,
-    defdelegateeverything: 1,
-    defdelegateeverything: 2,
-    defdelegatemacro: 2,
-    refute_defined: 2,
-    refute_defined: 3,
-    refute_macro: 2,
-    refute_macro: 3
+  export: [
+    locals_without_parens: locals_without_parens
   ],
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
   plugins: [Styler]
 ]
